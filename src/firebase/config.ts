68e484907f72dbf4ed3f07d44734141341f9aff8
Maijel Cancines <maijelcancines2@gmail.com>
2025-11-25 17:26:06 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "onboard-panel-gx822",
+  "appId": "1:872617759649:web:e2ade97d407b3498fc31e0",
+  "apiKey": "AIzaSyDmXiYc2YC0Y6PVxEJs7xLB_M73SfHtUEs",
+  "authDomain": "onboard-panel-gx822.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "872617759649"
+};
