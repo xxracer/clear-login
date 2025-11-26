aabd442a18f6a07a11bb129f462f0cfaf0bb21ae
Maijel Cancines <maijelcancines2@gmail.com>
2025-11-26 04:43:57 +0000

    pues te cuento que cree otro archivo json asi que es necesario que yo co

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
+
 export const firebaseConfig = {
-  "projectId": "onboard-panel-gx822",
-  "appId": "1:872617759649:web:e2ade97d407b3498fc31e0",
-  "apiKey": "AIzaSyDmXiYc2YC0Y6PVxEJs7xLB_M73SfHtUEs",
-  "authDomain": "onboard-panel-gx822.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "872617759649"
+  "projectId": "POR FAVOR REEMPLAZA ESTO",
+  "appId": "POR FAVOR REEMPLAZA ESTO",
+  "apiKey": "POR FAVOR REEMPLAZA ESTO",
+  "authDomain": "POR FAVOR REEMPLAZA ESTO",
+  "measurementId": "POR FAVOR REEMPLAZA ESTO",
+  "messagingSenderId": "POR FAVOR REEMPLAZA ESTO"
 };
