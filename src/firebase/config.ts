8b84021678d5858235fbdfa5f24e6d8203c03606
Maijel Cancines <maijelcancines2@gmail.com>
2025-11-26 14:19:22 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,14 +1,12 @@
 
 // Este objeto contiene la configuración PÚBLICA para la conexión del NAVEGADOR a Firebase.
 // Es seguro incluirlo en el código del cliente.
-// POR FAVOR, REEMPLAZA TODO EL CONTENIDO de firebaseConfig con el objeto de configuración de tu app web de Firebase.
-// Puedes encontrarlo en la Consola de Firebase > Configuración del Proyecto > Tus apps > App web.
 export const firebaseConfig = {
-  "apiKey": "POR FAVOR REEMPLAZA ESTO",
-  "authDomain": "POR FAVOR REEMPLAZA ESTO",
-  "projectId": "POR FAVOR REEMPLAZA ESTO",
-  "storageBucket": "POR FAVOR REEMPLAZA ESTO",
-  "messagingSenderId": "POR FAVOR REEMPLAZA ESTO",
-  "appId": "POR FAVOR REEMPLAZA ESTO",
-  "measurementId": "POR FAVOR REEMPLAZA ESTO"
+  apiKey: "AIzaSyDmXiYc2YC0Y6PVxEJs7xLB_M73SfHtUEs",
+  authDomain: "onboard-panel-gx822.firebaseapp.com",
+  databaseURL: "https://onboard-panel-gx822-default-rtdb.firebaseio.com",
+  projectId: "onboard-panel-gx822",
+  storageBucket: "onboard-panel-gx822.appspot.com",
+  messagingSenderId: "872617759649",
+  appId: "1:872617759649:web:e2ade97d407b3498fc31e0"
 };
