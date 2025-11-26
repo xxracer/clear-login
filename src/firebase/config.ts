b8373bf041b324a168e49240995b6f054c7b946a
Maijel Cancines <maijelcancines2@gmail.com>
2025-11-26 14:15:03 +0000

    estoy intentando iniciar sesion, y me da login error, y cuando le doy a

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,14 @@
 
+// Este objeto contiene la configuración PÚBLICA para la conexión del NAVEGADOR a Firebase.
+// Es seguro incluirlo en el código del cliente.
+// POR FAVOR, REEMPLAZA TODO EL CONTENIDO de firebaseConfig con el objeto de configuración de tu app web de Firebase.
+// Puedes encontrarlo en la Consola de Firebase > Configuración del Proyecto > Tus apps > App web.
 export const firebaseConfig = {
-  "projectId": "POR FAVOR REEMPLAZA ESTO",
-  "appId": "POR FAVOR REEMPLAZA ESTO",
   "apiKey": "POR FAVOR REEMPLAZA ESTO",
   "authDomain": "POR FAVOR REEMPLAZA ESTO",
-  "measurementId": "POR FAVOR REEMPLAZA ESTO",
-  "messagingSenderId": "POR FAVOR REEMPLAZA ESTO"
+  "projectId": "POR FAVOR REEMPLAZA ESTO",
+  "storageBucket": "POR FAVOR REEMPLAZA ESTO",
+  "messagingSenderId": "POR FAVOR REEMPLAZA ESTO",
+  "appId": "POR FAVOR REEMPLAZA ESTO",
+  "measurementId": "POR FAVOR REEMPLAZA ESTO"
 };
